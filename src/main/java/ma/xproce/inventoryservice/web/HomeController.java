358b27716c1a01dcdc0a912fdf90ecde75fcb46c
Mohammed_Pc <mohammed_pc@asus.com>
2024-04-01 00:03:08 +0000

    Added homepage HTML and configured routing

--- src/main/java/ma/xproce/inventoryservice/web/HomeController.java
+++ src/main/java/ma/xproce/inventoryservice/web/HomeController.java
@@ -1,4 +1,15 @@
 package ma.xproce.inventoryservice.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HomeController {
+    @GetMapping("/index")
+    public String Home() {
+        return "index";
+    }
+
+    @GetMapping("")
+    public String Home2() {
+        return "redirect:/index";
+    }
 }
